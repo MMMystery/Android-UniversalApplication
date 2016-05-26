de611ca2770ae1c4b2b5005b54c4d43ec304e9cc
unknown <>
2016-05-26 11:18:36 +0800

    因为融云需要服务端去获取token，所以在这里把融云去掉，改为bomb的im了

--- MyUniversalToolApplication2/app/src/main/java/com/example/administrator/myuniversaltoolapplication/ui/activity/LoginActivity.java
+++ MyUniversalToolApplication2/app/src/main/java/com/example/administrator/myuniversaltoolapplication/ui/activity/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.administrator.myuniversaltoolapplication.ui.activity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,27 +8,12 @@ import android.widget.EditText;
 
 import com.example.administrator.myuniversaltoolapplication.R;
 import com.example.administrator.myuniversaltoolapplication.app.AppConstants;
-import com.example.administrator.myuniversaltoolapplication.app.MyApplication;
-import com.example.administrator.myuniversaltoolapplication.entity.Friend;
 import com.example.administrator.myuniversaltoolapplication.entity.MyUser;
 import com.example.administrator.myuniversaltoolapplication.utils.SPUtils;
 import com.example.administrator.myuniversaltoolapplication.utils.ToastUtils;
-import com.orhanobut.logger.Logger;
-import com.squareup.okhttp.FormEncodingBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.listener.SaveListener;
-import io.rong.imkit.RongIM;
-import io.rong.imlib.RongIMClient;
-import io.rong.imlib.model.UserInfo;
 
 /**
  * Created by Administrator on 2016/1/16.
@@ -39,19 +23,11 @@ public class LoginActivity extends BaseActivity {
     private Button bt_register;
     private EditText et_username;
     private EditText et_Password;
-    private List<Friend> userIdList;
-    private static final String TOKEN = "L5q9BDBJrdSZL+bW2dQ3Q8YzH3VW73IW5fAgvgegI2eMEedG8biriWNuf21xiyb1xDBjDJoBgFYRhg+ASkpmJzNE3AyckuX6";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        userIdList = new ArrayList<Friend>();
-        userIdList.add(new Friend("f1195ce0a6", "a", "http://img.blog.csdn.net/20160219093419247"));
-        userIdList.add(new Friend("b248a15ad2", "b", "http://s1.jikexueyuan.com/my/images/empty/paizi-he-not-index-course.png"));
-
 
         initView();
     }
@@ -69,26 +45,8 @@ public class LoginActivity extends BaseActivity {
              //         * 获取本地用户信息
              //         */
 
-
-//            Logger.d(myUser.getAvater().getFileUrl(this));
-            Logger.d(myUser.getObjectId());
-            Logger.d(myUser.getUsername());
-            //去获取融云Token
-            new Thread() {
-                @Override
-                public void run() {
-                    super.run();
-
-
-//                        getToken("https://api.cn.ronghub.com/user/getToken.json", String.valueOf(myUser.getObjectId()), String.valueOf(myUser.getUsername()), "http://img2.imgtn.bdimg.com/it/u=3942935399,659074957&fm=21&gp=0.jpg");
-                    Logger.i("正在获取token");
-                    //连接融云服务器
-                    connect(TOKEN);
-
-//                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//                            startActivity(intent);
-                }
-            }.start();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
 
         } else {
             //第一次登陆，需要输入用户和密码
@@ -103,8 +61,6 @@ public class LoginActivity extends BaseActivity {
                         public void onSuccess() {
                             // TODO Auto-generated method stub
 
-
-//                            final String RM_Token_URL = "https://api.cn.ronghub.com/user/getToken.json";
                             String current_objectId = String.valueOf(myUser.getObjectId());
                             String current_Username = String.valueOf(myUser.getUsername());
 
@@ -113,21 +69,10 @@ public class LoginActivity extends BaseActivity {
                             //设置数据保存到本地SP中
                             SPUtils.put(getApplicationContext(), AppConstants.Key.CURRENT_OBJECTID, current_objectId);
                             SPUtils.put(getApplicationContext(), AppConstants.Key.CURRENT_USERNAME, current_Username);
-
-
-                            new Thread() {
-                                @Override
-                                public void run() {
-                                    super.run();
-//                                        getToken("https://api.cn.ronghub.com/user/getToken.json", String.valueOf(myUser.getObjectId()), String.valueOf(myUser.getUsername()), "http://img2.imgtn.bdimg.com/it/u=3942935399,659074957&fm=21&gp=0.jpg");
-                                    Logger.i("正在获取token");
-                                    //连接融云服务器
-//                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-//                                    finish();
-                                    connect(TOKEN);
-
-                                }
-                            }.start();
+                            //跳转到主界面
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            ToastUtils.show(getApplicationContext(),"登录成功");
                         }
 
 
@@ -151,7 +96,7 @@ public class LoginActivity extends BaseActivity {
         }
     }
 
-    OkHttpClient client = new OkHttpClient();
+  /*  OkHttpClient client = new OkHttpClient();
 
     String getToken(String url, String userId, String username, String img_avater) throws IOException {
         RequestBody body = new FormEncodingBuilder()
@@ -173,77 +118,7 @@ public class LoginActivity extends BaseActivity {
             Logger.i("获取token失败" + response.body().string());
             throw new IOException("Unexpected code " + response);
         }
-    }
-
-
-    private void connect(String token) {
-
-        if (getApplicationInfo().packageName.equals(MyApplication.getCurProcessName(getApplicationContext()))) {
-
-            /**
-             * IMKit SDK调用第二步,建立与服务器的连接
-             */
-            RongIM.connect(token, new RongIMClient.ConnectCallback() {
-
-                /**
-                 * Token 错误，在线上环境下主要是因为 Token 已经过期，您需要向 App Server 重新请求一个新的 Token
-                 */
-                @Override
-                public void onTokenIncorrect() {
-
-                    Logger.d("LoginActivity", "--onTokenIncorrect");
-                }
-
-                /**
-                 * 连接融云成功
-                 * @param userid 当前 token
-                 */
-                @Override
-                public void onSuccess(String userid) {
-
-                  /*  final String objectId = (String) SPUtils.get(getApplicationContext(), AppConstants.Key.OBJECTID, "////");
-                    final String currentusername = (String) SPUtils.get(getApplicationContext(), AppConstants.Key.CURRENT_USERNAME_KEY, "///");
-                    final String img_avater = (String) SPUtils.get(getApplicationContext(), AppConstants.Key.IMG_AVATER, "////");
-
-                    Logger.d("========",userid + "---" + objectId + "---" + currentusername + "---" + img_avater);*/
-                    //如果连接上服务，就提供当前用户的聊天信息（有哪些聊天）给融云
-                    RongIM.setUserInfoProvider(new RongIM.UserInfoProvider() {
-
-                        //其实本来要从融云那获取的userid和我从自己服务器端获取的objectId相等才可以执行下面，这才算是真正的关联上融云可以连接了。
-                        @Override
-                        public UserInfo getUserInfo(String userId) {
-                            Logger.d("userIdList---------" + userIdList.size());
-                            for (Friend i : userIdList) {
-                                if (i.getFriend_id().equals(userId)) {
-
-                                    Logger.d(i.getFriend_name().toString() + "/////" + i.getFriend_avater().toString());
-                                    return new UserInfo(i.getFriend_id(), i.getFriend_name(), Uri.parse(i.getFriend_avater()));
-                                }
-                            }
-                            return null;
-//                            return new UserInfo(objectId, currentusername, Uri.parse(img_avater));//根据 userId 去你的用户系统里查询对应的用户信息返回给融云 SDK。
-                        }
-
-                    }, true);
-
-                    ToastUtils.show(getApplicationContext(), "登录成功");
-                    Logger.d("LoginActivity", "--onSuccess" + userid);
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    finish();
-                }
-
-                /**
-                 * 连接融云失败
-                 * @param errorCode 错误码，可到官网 查看错误码对应的注释
-                 */
-                @Override
-                public void onError(RongIMClient.ErrorCode errorCode) {
-
-                    Logger.d("LoginActivity", "--onError" + errorCode);
-                }
-            });
-        }
-    }
+    }*/
 
 
 }
--- MyUniversalToolApplication2/app/src/main/java/com/example/administrator/myuniversaltoolapplication/ui/fragment/OneFragment.java
+++ MyUniversalToolApplication2/app/src/main/java/com/example/administrator/myuniversaltoolapplication/ui/fragment/OneFragment.java
@@ -1,31 +1,15 @@
 package com.example.administrator.myuniversaltoolapplication.ui.fragment;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bmob.BTPFileResponse;
-import com.bmob.BmobProFile;
-import com.bmob.btp.callback.UploadListener;
 import com.example.administrator.myuniversaltoolapplication.R;
-import com.example.administrator.myuniversaltoolapplication.ui.activity.ConversationActivity;
-import com.example.administrator.myuniversaltoolapplication.utils.ToastUtils;
-import com.orhanobut.logger.Logger;
-import com.squareup.picasso.Picasso;
-
-import cn.bmob.v3.datatype.BmobFile;
-import io.rong.imkit.RongIM;
-import io.rong.imkit.fragment.ConversationListFragment;
-import io.rong.imlib.model.Conversation;
 
 
 public class OneFragment extends Fragment implements View.OnClickListener {
@@ -39,94 +23,11 @@ public class OneFragment extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_one, container, false);
 
         initView(view);
-        //引入会话列表
-        enterConversationListFragment();
-
-
-      /*  //上传文件
-        String filePath = "sdcard/aa.jpg";
-        BTPFileResponse response = BmobProFile.getInstance(getContext()).upload(filePath, new UploadListener() {
-
-            @Override
-            public void onSuccess(String fileName,String url,BmobFile file) {
-                Logger.d("bmob","文件上传成功："+fileName+",可访问的文件地址："+file.getUrl());//后面这个就是访问图片的url不过没有在bmob进行url签名暂时访问时无效的
-
-                ToastUtils.show(getActivity(),"新方法---上传文件成功：" + file.getUrl());
-
-                //获取可以网页真正直接访问的图片的url
-
-                String URL1 =BmobProFile.getInstance(getActivity()).signURL(fileName,file.getUrl(),"7af54fcbd534962f74d6f18d30441aea",0,null);
-                Logger.d(URL1);
-                String URL2 = BmobProFile.getInstance(getActivity()).signURL(fileName,file.getUrl(),"7af54fcbd534962f74d6f18d30441aea",50,"abc");
-                Logger.d(URL2);
-                Picasso.with(getActivity())
-                        .load(URL1)
-                        .error(R.mipmap.ic_launcher)
-                        .into(fragment_one_iv_three);
-
-                //下载文件
-                BmobProFile.getInstance(getApplicationContext()).download(fileName, new DownloadListener() {
-
-                    @Override
-                    public void onSuccess(String fullPath) {
-                        // TODO Auto-generated method stub
-                        //这里是真的把文件下载下来了。
-                        Logger.d("bmob下载成功："+fullPath);//fullPath打印出来是：/data/data/com.example.administrator.myuniversaltoolapplication/cache/BmobCache/Download/5089eded673e46a8aa4bddeacfffb91d.jpg
-                    }
-
-                    @Override
-                    public void onProgress(String localPath, int percent) {
-                        // TODO Auto-generated method stub
-                        Logger.d("bmob","download-->onProgress :"+percent);
-//                        dialog.setProgress(percent);
-                    }
-
-                    @Override
-                    public void onError(int statuscode, String errormsg) {
-                        // TODO Auto-generated method stub
-//                        dialog.dismiss();
-                        Logger.d("bmob","下载出错："+statuscode +"--"+errormsg);
-                    }
-                });*//*
-                // TODO Auto-generated method stub
-                // fileName ：文件名（带后缀），这个文件名是唯一的，开发者需要记录下该文件名，方便后续下载或者进行缩略图的处理
-                // url        ：文件地址
-                // file        :BmobFile文件类型，`V3.4.1版本`开始提供，用于兼容新旧文件服务。
-                //注：若上传的是图片，url地址并不能直接在浏览器查看（会出现404错误），需要经过`URL签名`得到真正的可访问的URL地址,当然，`V3.4.1`的版本可直接从'file.getUrl()'中获得可访问的文件地址。
-            }
-
-
-
-            @Override
-            public void onProgress(int progress) {
-                // TODO Auto-generated method stub
-                Log.i("bmob","onProgress :"+progress);
-            }
-
-            @Override
-            public void onError(int statuscode, String errormsg) {
-                // TODO Auto-generated method stub
-                Log.i("bmob","文件上传失败："+errormsg);
-            }
-        });*/
 
         return view;
 
     }
 
-    private void enterConversationListFragment() {
-        //ConversationListFragment是融云自带的fragment,嵌入进来了
-        ConversationListFragment fragment = (ConversationListFragment) getChildFragmentManager().findFragmentById(
-                R.id.conversationlist);
-
-        Uri uri = Uri.parse("rong://" + getActivity().getApplicationInfo().packageName).buildUpon().appendPath("conversationlist")
-                .appendQueryParameter(Conversation.ConversationType.PRIVATE.getName(), "false") // 设置私聊会话非聚合显示
-                .appendQueryParameter(Conversation.ConversationType.GROUP.getName(), "true")// 设置群组会话聚合显示
-                .appendQueryParameter(Conversation.ConversationType.DISCUSSION.getName(), "false")// 设置讨论组会话非聚合显示
-                .appendQueryParameter(Conversation.ConversationType.SYSTEM.getName(), "false")// 设置系统会话非聚合显示
-                .build();
-        fragment.setUri(uri);
-    }
 
 
     private void initView(View view) {
@@ -147,10 +48,6 @@ public class OneFragment extends Fragment implements View.OnClickListener {
                 Toast.makeText(getActivity(), "left", Toast.LENGTH_LONG).show();
                 break;
             case R.id.topbar_btn_right:
-                //启动会话界面
-                if (RongIM.getInstance() != null)
-                    RongIM.getInstance().startPrivateChat(getActivity(), "f1195ce0a6", "title");//这里控制了和谁对话，启动ConversationActivity。
-
                 break;
         }
 
